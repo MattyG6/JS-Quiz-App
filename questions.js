7126fb139ef1a419035fe55f40d4ab0627312be5
MithrandirG6 <mathewjgracie@gmail.com>
2022-11-27 11:18:42 -0400

    add js files and screenshot for readme

---
+++ questions.js
@@ -0,0 +1,57 @@
+let questions = [
+    {
+    numb: 1,
+    question: "How are Java and Javascript related?",
+    answer: "They aren't related",
+    options: [
+      "They are both owned and updated by JavaCorporation",
+      "Javascript does all front-end work, Java does all back-end work",
+      "They aren't related",
+      "They're named after the father of web development, Ernst Java"
+    ]
+  },
+    {
+    numb: 2,
+    question: "What does CSS stand for?",
+    answer: "Cascading Style Sheets",
+    options: [
+      "Cascading Style Sheets",
+      "Cornerstone Styling Scale",
+      "Crandall's Styling System",
+      "Cool Stuff Slides"
+    ]
+  },
+    {
+    numb: 3,
+    question: "What is the name for a group of elemts housed in [] brackets in Javascript?",
+    answer: "Array",
+    options: [
+      "Function",
+      "Array",
+      "Object",
+      "Loop"
+    ]
+  },
+    {
+    numb: 4,
+    question: "What symbol do you use to declare universal stylings?",
+    answer: "* {}",
+    options: [
+      "# {}",
+      "|| {}",
+      "& {}",
+      "* {}"
+    ]
+  },
+    {
+    numb: 5,
+    question: "What does URL stand for?",
+    answer: "Uniform Resource Locator",
+    options: [
+      "Uniform Resource Locator",
+      "Unified Rendering Locality",
+      "Undeclared Resource Location",
+      "Uniform Relative Link"
+    ]
+  },
+];
